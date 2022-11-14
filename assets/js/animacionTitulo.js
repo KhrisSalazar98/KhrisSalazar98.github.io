1c914a891287086c6a42ac29dc6bd7c761c10c66
Cristóbal Salazar <68259718+KhrisSalazar98@users.noreply.github.com>
2022-11-14 00:37:35 -0300

    corrigiendo errores

--- assets/js/animacionTitulo.js
+++
@@ -1,7 +0,0 @@
-const tituloLetras = document.querySelectorAll(".tituloLetra");
-        
-for(let i = 0; i<tituloLetras.length; i++){        
-    setTimeout(()=>{
-        tituloLetras[i].style.opacity = "1";
-    },200*(i+1));
-}
